bee83d5d142a8ddfaf0cb5c90993a3937ddd9bcf
Kacper Budny <kacperbudny98@gmail.com>
2021-04-07 23:46:33 +0200

    Add games controller and view Add very basic styling for games view Seed some games into database Add cover images

--- GameWeb/Data/ApplicationDbContext.cs
+++ GameWeb/Data/ApplicationDbContext.cs
@@ -16,5 +16,45 @@ namespace GameWeb.Data
         }
         public DbSet<ApplicationUser> ApplicationUser { get; set; }
         public DbSet<Game> Game { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Game>().HasData(
+                new Game
+                {
+                    Id = 1,
+                    Name = "Minecraft",
+                    ReleaseDate = new DateTime(2011, 11, 18),
+                    Platform = "PC, Xbox, PlayStation, Android",
+                    Publisher = "Mojang",
+                    Description = "Lorem ipsum",
+                    Image = "1.jpg",
+                    Genre = "Sandbox"
+                },
+                new Game
+                {
+                    Id = 2,
+                    Name = "Rayman 3: Hoodlum Havoc",
+                    ReleaseDate = new DateTime(2003, 02, 15),
+                    Platform = "PC, Xbox, PlayStation",
+                    Publisher = "Ubisoft",
+                    Description = "Lorem ipsum",
+                    Image = "1.jpg",
+                    Genre = "Platformówki"
+                },
+                new Game
+                {
+                    Id = 3,
+                    Name = "FlatOut 2",
+                    ReleaseDate = new DateTime(2006, 06, 30),
+                    Platform = "PC, Xbox, PlayStation",
+                    Publisher = "Empire Interactive",
+                    Description = "Lorem ipsum",
+                    Image = "3.jpg",
+                    Genre = "Wyœcigi"
+                }
+                );
+        }
     }
 }
