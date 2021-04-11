760db421681550e7a6e342ebb30f962963a4ab34
piotr-eryk <52891380+piotr-eryk@users.noreply.github.com>
2021-04-11 19:15:45 +0200

    Merge two models

--- GameWeb/Models/ApplicationUser.cs
+++ GameWeb/Models/ApplicationUser.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,31 @@ namespace GameWeb.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required(ErrorMessage = "Please enter your first name")]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a strong password")]
+        [Compare("ConfirmPassword", ErrorMessage ="Password does not match")]
+        [Display(Name ="Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+
         public DateTime BirthDate { get; set; }
         public string Description { get; set; }
         [NotMapped]
         public string Role { get; set; }
+
+
+
     }
 }
