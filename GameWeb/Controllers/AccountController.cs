861b76e9b3847a36ae4a34eb88b37545eac85f7d
piotr-eryk <52891380+piotr-eryk@users.noreply.github.com>
2021-04-12 12:45:20 +0200

    Add register model

--- GameWeb/Controllers/AccountController.cs
+++ GameWeb/Controllers/AccountController.cs
@@ -5,14 +5,62 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using GameWeb.Models;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace GameWeb.Controllers
 {
     public class AccountController : Controller
     {
-        [Route("signup")]
-        public IActionResult Signup()
+private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IIdentityServerInteractionService _interactionService;
+
+        public AuthController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            IIdentityServerInteractionService interactionService)
         {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _interactionService = interactionService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login(string returnUrl)
+        {
+            var externalProviders = await _signInManager.GetExternalAuthenticationSchemesAsync();
+            return View(new LoginViewModel
+            {
+                ReturnUrl = returnUrl,
+                ExternalProviders = externalProviders
+            });
+        }
+
+        [HttpGet]
+        public IActionResult Register(string returnUrl)
+        {
+            return View(new RegisterViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            var user = new IdentityUser(vm.Username);
+            var result = await _userManager.CreateAsync(user, vm.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+
+                return Redirect(vm.ReturnUrl);
+            }
+
             return View();
         }
     }
