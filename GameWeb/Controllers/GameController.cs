74c0272e904a2dac6543a18bdda713f152d9e56e
maggy98 <62389300+maggy98@users.noreply.github.com>
2021-04-27 17:37:29 +0200

    edit without image

--- GameWeb/Controllers/GameController.cs
+++ GameWeb/Controllers/GameController.cs
@@ -107,5 +107,32 @@ namespace GameWeb.Controllers
 
 
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Game.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Game obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Game.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
