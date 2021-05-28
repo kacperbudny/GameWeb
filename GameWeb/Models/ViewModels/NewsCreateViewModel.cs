f2bbe343938b412b76bfa588cfe7e5b71a3e1166
kacperbudny <kacperbudny98@gmail.com>
2021-05-28 23:23:06 +0200

    Add NewsController with basic implementations of Index and Create

---
+++ GameWeb/Models/ViewModels/NewsCreateViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameWeb.Models.ViewModels
+{
+    public class NewsCreateViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Proszę wprowadzić tytuł.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Proszę wprowadzić zawartość.")]
+        public string Content { get; set; }
+        [Required]
+        public DateTime PublicationDate { get; set; }
+        [Required]
+        public string AuthorID { get; set; }
+        public ApplicationUser Author { get; set; }
+        public string Tags { get; set; }
+        [Required(ErrorMessage = "Proszę umieścić zdjęcie.")]
+        public virtual IFormFile ImageFile { get; set; }
+    }
+}
