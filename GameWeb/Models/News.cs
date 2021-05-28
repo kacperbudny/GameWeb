fc426b2ff1fb3a3a4b2990134cfa67f1142ea818
kacperbudny <kacperbudny98@gmail.com>
2021-05-28 23:00:15 +0200

    Add News model

---
+++ GameWeb/Models/News.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameWeb.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Proszę wprowadzić tytuł.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Proszę wprowadzić zawartość.")]
+        public string Content { get; set; }
+        [Required]
+        public DateTime PublicationDate { get; set; }
+        [Required]
+        public string AuthorID { get; set; }
+        public ApplicationUser Author { get; set; }
+        public string Tags { get; set; }
+        [Required(ErrorMessage = "Proszę umieścić zdjęcie.")]
+        public string ImagePath { get; set; }
+    }
+}
